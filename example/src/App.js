5f3f51ff3090174e9cf646d39b467b1be780f2d4
Leyla Becker <git@jan-leila.com>
2022-10-03 23:58:46 -0500

    created crumb lib

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,115 @@
-import React from 'react'
+import React, { Fragment, useMemo } from 'react'
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 
-import { ExampleComponent } from 'react-bread-crumb'
-import 'react-bread-crumb/dist/index.css'
+import { BreadcrumbProvider, useCrumb, useTrailView, useRootCrumb } from 'react-bread-crumb'
+
+const Home = () => {
+  const crumb = useMemo(() => {
+    return {
+      text: "home",
+      path: "/",
+    }
+  }, [])
+  useRootCrumb(crumb)
+
+  return (
+    <div>
+      <Link to="/page1">page1</Link>
+      <Link to="/page2">page2</Link>
+      <Link to="/page3">page3</Link>
+    </div>
+  )
+}
+
+const Page1 = () => {
+  const crumb = useMemo(() => {
+    return {
+      text: "page 1",
+      path: "/page1",
+    }
+  }, [])
+  useCrumb(crumb, "page 1")
+
+  return (
+    <div>
+      Page 1
+      <Link to="/page2">page2</Link>
+      <Link to="/page3">page3</Link>
+    </div>
+  )
+}
+
+const Page2 = () => {
+  const crumb = useMemo(() => {
+    return {
+      text: "page 2",
+      path: "/page2",
+    }
+  }, [])
+  useCrumb(crumb, "page 2")
+
+  return (
+    <div>
+      Page 2
+    </div>
+  )
+}
+
+const Page3 = () => {
+  const crumb = useMemo(() => {
+    return {
+      text: "page 3",
+      path: "/page3",
+    }
+  }, [])
+  useCrumb(crumb, "page 3")
+
+  return (
+    <div>
+      Page 3
+    </div>
+  )
+}
+
+const BreadCrumb = () => {
+  const trail = useTrailView()
+
+  return (
+    <>
+      <div>
+        {
+          trail.map((crumb, index) => {
+            return (
+              <Fragment key={index}>
+                { index !== 0 && <span>{">"}</span>}
+                {
+                  <Link to={crumb.path}>{crumb.text}</Link>
+                }
+              </Fragment>
+            )
+          })
+        }
+      </div>
+      <Outlet />
+    </>
+  )
+}
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <React.StrictMode>
+    <BreadcrumbProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<BreadCrumb />}>
+            <Route index element={<Home />} />
+            <Route path="page1" element={<Page1 />}/>
+            <Route path="page2" element={<Page2 />}/>
+            <Route path="page3" element={<Page3 />}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </BreadcrumbProvider>
+  </React.StrictMode>
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,128 @@
-import React from 'react'
-import styles from './styles.module.css'
+import React, { createContext, useCallback, useContext, useEffect, useId, useMemo, useReducer, useRef, useState } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const breadcrumbContext = createContext([])
+
+const ADD_CRUMB = Symbol()
+const RESET_TRAIL = Symbol()
+
+export const BreadcrumbProvider = ({ children, trail: initial_trail = [] }) => {
+  const [ count, setCount ] = useState(0)
+  const [ trail, dispatch ] = useReducer((trail, { action, crumb }) => {
+    switch(action){
+      case ADD_CRUMB:
+        const index = crumb.id && trail.findIndex(({ id }) => {
+          return id && id === crumb.id
+        })
+        if(index === -1) {
+          return [ ...trail, crumb ]
+        }
+        return trail.slice(0, index + 1)
+      case RESET_TRAIL:
+        return [ crumb ]
+      default:
+        return trail
+    }
+  }, initial_trail)
+
+  const addCrumb = useCallback((id, crumb) => {
+    dispatch({
+      action: ADD_CRUMB,
+      crumb: {
+        id, crumb,
+      },
+    })
+    setCount((count) => count + 1)
+  }, [dispatch])
+
+  const resetTrail = useCallback((crumb) => {
+    dispatch({
+      action: RESET_TRAIL,
+      crumb: { crumb, },
+    })
+    setCount((count) => count + 1)
+  }, [dispatch])
+
+  const unloadCrumb = useCallback(() => {
+    setCount((count) => count - 1)
+  }, [])
+
+  const trailView = useMemo(() => {
+    return trail.map(({ crumb }) => crumb)
+  }, [trail])
+  
+  return (
+    <breadcrumbContext.Provider value={
+      {
+        trail,
+        trailView,
+        addCrumb,
+        resetTrail,
+        unloadCrumb,
+        count,
+      }
+    }>
+      {children}
+    </breadcrumbContext.Provider>
+  )
+}
+export const useRootCrumb = (crumb) => {
+  const { resetTrail, unloadCrumb, count } = useContext(breadcrumbContext)
+  
+  const ref = useRef(crumb)
+  const initialCrumb = ref.current === crumb
+
+  useEffect(() => {
+    if (!initialCrumb)
+      console.trace("Crumb changing during lifetime of object, make sure to memoize it")
+  }, [initialCrumb])
+
+  useEffect(() => {
+    if (count > 1)
+      console.trace("Multiple crumb locations being used at one time")
+  }, [count])
+
+  useEffect(() => {
+    resetTrail(ref.current)
+    return unloadCrumb
+  }, [resetTrail, ref.current, unloadCrumb])
+}
+export const RootCrumb = (crumb) => {
+  useCrumb(crumb)
+  return <></>
+}
+
+export const useCrumb = (crumb, id) => {
+  const { addCrumb, unloadCrumb, count } = useContext(breadcrumbContext)
+
+  const ref = useRef(crumb)
+  const initialCrumb = ref.current === crumb
+
+  useEffect(() => {
+    if (!initialCrumb)
+      console.trace("Crumb changing during lifetime of object, make sure to memoize it")
+  }, [initialCrumb])
+
+  useEffect(() => {
+    if(count > 1)
+      console.trace("Multiple crumb locations being used at one time")
+  }, [count])
+
+  useEffect(() => {
+    addCrumb(id, ref.current)
+    return unloadCrumb
+  }, [addCrumb, id, ref.current, unloadCrumb])
+}
+export const Crumb = (crumb, id) => {
+  useCrumb(crumb, id)
+  return <></>
+}
+
+export const useTrailView = () => {
+  const { trailView } = useContext(breadcrumbContext)
+  return trailView
+}
+
+export const useTrail = () => {
+  const { trail } = useContext(breadcrumbContext)
+  return trail
 }
